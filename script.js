1d73f68019dbd1cba285a54e95175dc60299d8cb
terracelover <160361065+terracelover@users.noreply.github.com>
2024-02-17 22:25:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openNoImage() {
+  window.open(
+    "https://tenor.com/view/sad-thump-up-cat-sad-cat-sad-thumb-up-gif-20650301",
+    "_blank"
+  );
+}
+
+function openYesImage() {
+  window.open(
+    "https://tenor.com/view/yippee-iluvmybf-gif-4712057245981450122",
+    "_blank"
+  );
+}
